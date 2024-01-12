864b59cb5ed25f80fbce01b06b732017ab394a64
Pedro Santos <espedrosantos@gmail.com>
2024-01-12 11:18:59 -0300

    feat: Implementação de redirecionamento de campanhas

--- script.js
+++ script.js
@@ -1,76 +1,185 @@
-let linksArray = [];
+  let linksArray = [];
 
-function adicionarLink() {
-  const linksContainer = document.getElementById('linksContainer');
-  const newLinkInput = document.createElement('div');
-  newLinkInput.classList.add('link-input');
+  function salvarDadoNoCache(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
 
-  const newLink = document.createElement('input');
-  newLink.type = 'text';
-  newLink.name = 'links';
-  newLink.required = true;
-  newLink.placeholder = 'Digite o link';
-  newLinkInput.appendChild(newLink);
+  function buscarDadoDoCache(key) {
+    var dado = JSON.parse(localStorage.getItem(key));
+    return dado;
+  }
 
-  linksContainer.appendChild(newLinkInput);
-}
+  function adicionarLink() {
+    const linksContainer = document.getElementById('linksContainer');
+    const newLinkInput = document.createElement('div');
+    newLinkInput.classList.add('link-input');
 
-function cadastrarCampanha() {
-  const maxCliques = document.getElementById('maxCliques').value;
+    const newLink = document.createElement('input');
+    newLink.type = 'text';
+    newLink.name = 'links';
+    newLink.required = true;
+    newLink.placeholder = 'Digite o link';
+    newLinkInput.appendChild(newLink);
 
-  const linkInputs = document.querySelectorAll('.link-input input');
-  linksArray = Array.from(linkInputs).map(input => input.value);
+    linksContainer.appendChild(newLinkInput);
+  }
+
+  function cadastrarCampanha() {
+    const maxCliques = document.getElementById('maxCliques').value;
+
+    const linkInputs = document.querySelectorAll('.link-input input');
+    linksArray = Array.from(linkInputs).map(input => input.value);
+
+    const campanhaData = {
+      maximoDeCliques: parseInt(maxCliques),
+      links: linksArray
+    };
+
+    fetch('https://campanhas-api.azurewebsites.net/Campanha', {
+      method: 'POST',
+      headers: {
+          'origin': 'any',
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(campanhaData)
+    })
+    .then(response => {
+      if (response.status === 201) {
+        return response.json();
+      } else {
+        throw new Error('Erro ao cadastrar campanha');
+      }
+    })
+    .then(data => {
+      gerarEventoSucesso(data);
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+  }
+
+
+  function gerarEventoSucesso(data) {
+      alert('Criado campanha com id:' + data.id +  ' com sucesso')
+
+      addTableRow(data.id, data.linkDeAcesso)
+    }
+
+    function addTableRow(id, link) {
+      var tableBody = document.getElementById('itensTable').getElementsByTagName('tbody')[0];
+
+      var newRow = tableBody.insertRow(tableBody.rows.length);
+      var cell1 = newRow.insertCell(0);
+      var cell2 = newRow.insertCell(1);
+
+      cell1.textContent = id;
+      cell2.textContent = link;
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+      carregarCampanhasDropdown();
+    });
+    
+    function carregarCampanhasDropdown() {
+      const campanhaDropdown = document.getElementById('campanhaDropdown');
+      const selectedCampanhaLink = campanhaDropdown.value;
+
+      fetch('https://campanhas-api.azurewebsites.net/Campanha')
+        .then(response => response.json())
+        .then(data => {
+          campanhaDropdown.innerHTML = '<option value="" disabled selected>Selecione uma campanha</option>';
+          
+          salvarDadoNoCache('listaCampanhas', data)
+
+          data.forEach(campanha => {
+            const option = document.createElement('option');
+            option.value = campanha.linkDeAcesso;
+            option.textContent = campanha.linkDeAcesso;
+            campanhaDropdown.appendChild(option);
+          });
+          
+          campanhaDropdown.value = selectedCampanhaLink;
+        })
+        .catch(error => {
+          console.error('Erro ao carregar campanhas:', error);
+        });
+    }
+    
+    function carregarCampanha() {
+      const campanhaDropdown = document.getElementById('campanhaDropdown');
+      const selectedCampanhaLink = campanhaDropdown.value;
+    
+      const campanhaDetails = document.getElementById('campanhaDetails');
+    
+      campanhaDetails.innerHTML = '';
+    
+      if (selectedCampanhaLink) {
+        fetch(`https://campanhas-api.azurewebsites.net/Campanha`)
+        .then(response => response.json())
+        .then(data => {
+          let campanhasFilter = data.filter(function(x) {
+            return x.linkDeAcesso === selectedCampanhaLink;
+          })          
+          
+          let campanha = campanhasFilter[0]
+          
+          const detailsParagraph = document.createElement('p');
+          detailsParagraph.textContent = `${campanha.linkDeAcesso}`;
+          campanhaDetails.appendChild(detailsParagraph);
+          
+          const urisList = document.createElement('ul');
+          
+          campanha.links.forEach(link => {
+            const listItem = document.createElement('li');
+            
+            listItem.innerHTML = `<span style="float: left;">${link.url}</span><span style="float: right;">${link.cliquesRecebidos}/${campanha.maximoDeCliques}</span>`;
+            urisList.appendChild(listItem);
+            campanhaDetails.appendChild(urisList);
+          })
+
+          salvarDadoNoCache('listaCampanhas', data)
+
+          const btnRedirect = document.createElement('button');
+          btnRedirect.id = 'btnRedirect';
+          btnRedirect.textContent = 'Acessar Campanha';
+
+          btnRedirect.addEventListener("click", RedirecionarCampanha);
+          campanhaDetails.appendChild(btnRedirect);
+        })
+        .catch(error => {
+          console.error('Erro ao carregar campanhas:', error);
+        });
+      }
+    }
+    
+ function RedirecionarCampanha() {
+  
+  const campanhaDropdown = document.getElementById('campanhaDropdown');
+  const selectedCampanhaLink = campanhaDropdown.value;
 
   const campanhaData = {
-    maximoDeCliques: parseInt(maxCliques),
-    links: linksArray
+    linkDeAcesso: selectedCampanhaLink
   };
 
-  fetch('https://campanhas-api.azurewebsites.net/Campanha', {
-    method: 'POST',
+  fetch('https://campanhas-api.azurewebsites.net/Campanha/Redirecionar', {
+    method: 'PUT',
     headers: {
-        'origin': 'any',
+      'origin': 'any',
       'accept': 'application/json',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(campanhaData)
   })
   .then(response => {
-    if (response.status === 201) {
-      return response.json();
+    if (response.status === 200) {
+      carregarCampanha()
     } else {
       throw new Error('Erro ao cadastrar campanha');
     }
   })
-  .then(data => {
-    gerarEventoSucesso(data);
-  })
   .catch(error => {
-    alert(error.message);
+    alert('Erro ao fazer redirecionamento de campanha');
   });
-}
 
-
-function gerarEventoSucesso(data) {
-    alert('Criado campanha com id:' + data.id +  ' com sucesso')
-
-    // const itensCriadosElement = document.getElementById('itensCriados')
-
-    // var itemCriadoParagraph = document.createElement('tr')
-    // itemCriadoParagraph.textContent = `ID: ${data.id} | Link de Acesso: ${data.linkDeAcesso}`
-    
-    // itensCriadosElement.append(itemCriadoParagraph)
-
-    addTableRow(data.id, data.linkDeAcesso)
-  }
-
-  function addTableRow(id, link) {
-    var tableBody = document.getElementById('itensTable').getElementsByTagName('tbody')[0];
-
-    var newRow = tableBody.insertRow(tableBody.rows.length);
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-
-    cell1.textContent = id;
-    cell2.textContent = link;
-  }
+ }
